c5962b4039ae7c920d9af775194bd1beedc02db1
Ismael pereira <nilsonjoao647@gmail.com>
2026-05-08 12:31:06 -0100

    minhas alteraçôes

--- app/(auth)/Home/page.tsx
+++ app/(auth)/Home/page.tsx
@@ -3,7 +3,7 @@ import { PedidoCard } from "@/components/core/pedido-card"
 export default function Home() {
     return (
         <div>
-            <h1>Home</h1>
+            <h1>k</h1>
             <PedidoCard
                 title="Instalar porta de entrada"
                 description="preciso instalar uma porta de entrada em uma casa"
